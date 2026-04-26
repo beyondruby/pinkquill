3d3e2c41b5af4bf64f4656395405e942512398a4
beyondruby <88088605+beyondruby@users.noreply.github.com>
2026-04-26 15:57:13 +0300

    fix: error login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import AuthForm from "@/components/auth/AuthForm";
 
 export default function LoginPage() {
-  return <AuthForm />;
+  return (
+    <Suspense fallback={null}>
+      <AuthForm />
+    </Suspense>
+  );
 }
